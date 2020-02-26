cdbb37023e4941f6fbd939f3f4b81be251365020
Bryn <thenazboz@gmail.com>
2020-02-26 19:19:07 +0000

    Done.

--- db/migrate/20140922151444_create_tadpoles.rb
+++ db/migrate/20140922151444_create_tadpoles.rb
@@ -1,3 +1,8 @@
 class CreateTadpoles < ActiveRecord::Migration
-  # code goes here
+    def change
+      create_table :ponds do |t|
+      t.string :water_type
+      t.string :name
+    end
+end
 end
